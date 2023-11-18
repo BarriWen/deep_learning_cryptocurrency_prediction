4ac2f7a1b0e9ca35fdcb8fb9b0e5a80179a7c8e3
BarriWen <wenzelongkyrie@outlook.com>
2023-11-17 22:15:45 -0500

    change  dataset

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ import tensorflow_decision_forests as tfdf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+print("TensorFlow Decision Forests v" + tfdf.__version__)
+print("TensorFlow Addons v" + tfa.__version__)
+print("TensorFlow v" + tf.__version__)
